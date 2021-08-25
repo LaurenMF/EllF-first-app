492d7b1028ea13c2404f22b7a84e645224a4f851
LaurenMF <lfasig@iu.edu>
2021-08-25 16:55:25 -0400

    master

---
+++ src/pages/products/games.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const GamesPage = () => {
+    return (
+        <React.Fragment>
+            <h1>Games</h1>
+            
+        </React.Fragment>
+    );
+}
+
+export default GamesPage;
