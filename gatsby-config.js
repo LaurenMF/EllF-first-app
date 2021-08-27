a5073c1f7dced9d0170914d33b980fe54809d80b
LaurenMF <lfasig@iu.edu>
2021-08-26 21:25:10 -0400

    first app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~lfasig/N322/EllF-first-app",
     title: "First App",
   },
   plugins: [],
+  pathPreFix: '/~lfasig/N322/EllF-first-app'
 };
